06b48e668c94f696799d817775d67a9ff9229c2f
吴泽瑞 <jjj@dareway.com.cn>
2024-10-22 13:46:37 +0800

    init

---
+++ src/main/java/com/example/airspaceback/AirspaceBackApplication.java
@@ -0,0 +1,12 @@
+package com.example.airspaceback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class AirspaceBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirspaceBackApplication.class, args);
+    }
+
+}
